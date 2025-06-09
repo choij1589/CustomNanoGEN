842055166ff13ad97a1ad550fc49a0b1acbe483e
Jin Choi <choij@cern.ch>
2025-06-09 17:10:41 +0900

    fix gridpack path

--- configs/Hadronizer_TuneCP5_13p6TeV_DY3j_LO_5f_CUDA_LHE_pythia8_cfg.py
+++ configs/Hadronizer_TuneCP5_13p6TeV_DY3j_LO_5f_CUDA_LHE_pythia8_cfg.py
@@ -177,7 +177,7 @@ process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     nEvents = cms.untracked.uint32(10000),
     numberOfParameters = cms.uint32(2),
     outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd.sh')
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd_autodetect.sh')
 )
 
 
--- python/Hadronizer_TuneCP5_13p6TeV_DY3j_LO_5f_CUDA_LHE_pythia8_cff.py
+++ python/Hadronizer_TuneCP5_13p6TeV_DY3j_LO_5f_CUDA_LHE_pythia8_cff.py
@@ -6,7 +6,7 @@ externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(2),
     outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd.sh'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd_autodetect.sh'),
     generateConcurrently = cms.untracked.bool(True),
 )
 
