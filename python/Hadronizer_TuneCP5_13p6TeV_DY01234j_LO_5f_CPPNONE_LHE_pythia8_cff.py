d709caea1fbce0fbc1bef8f076e0654888c0d232
Jin Choi <choij@cern.ch>
2025-06-24 17:04:23 +0900

    Update hadronizers for DY01234j

---
+++ python/Hadronizer_TuneCP5_13p6TeV_DY01234j_LO_5f_CPPNONE_LHE_pythia8_cff.py
@@ -0,0 +1,47 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
+    args = cms.vstring('root://eosuser.cern.ch//eos/user/c/choij/public/MG4GPU/gridpacks/validation/DY01234j/DY01234j_LO_5f_CPPNONE_el9_amd64_gcc11_CMSSW_13_2_9_tarball.tar.xz',
+                       '5000'),  # maxevt - maximum events per job
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(2),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd_autodetect.sh'),
+    generateConcurrently = cms.untracked.bool(False),
+)
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            'JetMatching:setMad = off',
+            'JetMatching:scheme = 1',
+            'JetMatching:merge = on',
+            'JetMatching:jetAlgorithm = 2',
+            'JetMatching:etaJetMax = 5.',
+            'JetMatching:coneRadius = 1.',
+            'JetMatching:slowJetPower = 1',
+            'JetMatching:doShowerKt = off', 
+            'JetMatching:qCut = 19.',
+            'JetMatching:nQmatch = 5',
+            'JetMatching:nJetMax = 4',
+            'TimeShower:mMaxGamma = 4.0'
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters',
+            'pythia8PSweightsSettings'
+        )
+    ),
+    comEnergy = cms.double(13600),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+)
--- scripts/runLocalCmsDriverNanoGen.sh
+++ scripts/runLocalCmsDriverNanoGen.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 fragment=${1/python\//}
 nevt=$2
+nthreads=$3
 SEED=$(($(date +%s) % 100 + 1))
 
-# Run3
-cmsDriver.py Configuration/CustomNanoGEN/python/$fragment --python_filename localTest/NANOGEN/${fragment/cff/cfg} \
+cmsDriver.py Configuration/CustomNanoGEN/python/$fragment \
+    --python_filename configs/${fragment/cff/cfg} \
     --eventcontent NANOAODGEN --customise Configuration/DataProcessing/Utils.addMonitoring \
-    --datatier NANOAOD --fileout file:NANOGEN.root --conditions 130X_mcRun3_2023_realistic_v14 \
-    --beamspot Realistic25ns13p6TeVEarly2023Collision \
-    --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})" \
-                         process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(243)" \
-    --step LHE,GEN,NANOGEN --eventcontent LHE,RAWSIM,NANOAODSIM --datatier LHE,GEN,NANOAOD \
-    --geometry DB:Extended --era Run3_2023 \
-    --no_exec --mc -n $nevt || exit $?;
+    --datatier NANOAOD --fileout file:NANOGEN.root --mc \
+    --conditions auto:mc --step LHE,GEN,NANOGEN -n $nevt --nThreads $nthreads --no_exec || exit $?
